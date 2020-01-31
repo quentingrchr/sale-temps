623f04e51922dfb8aaa2061ac49aa60366e8a9cc
Quentin GRANCHER <quentingrancher@ANTI-SNITCH-SNITCH-CLUB.local>
2020-02-01 00:00:40 +0100

    dark mode and carousel

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,5 +1,9 @@
 const $cards = document.querySelectorAll(".card");
 const $cardBtn = document.querySelectorAll(".card__btn");
+const $body = document.querySelector("body");
+const $liquidsTexts = document.querySelectorAll(".liquids__content__text");
+const $liquidsTypes = document.querySelectorAll(".liquids__content__type");
+const $liquidsImgsBoxs = document.querySelectorAll(".liquids__content__box");
 
 for (let i = 0; i < $cardBtn.length; i++) {
   const element = $cardBtn[i];
@@ -8,18 +12,24 @@ for (let i = 0; i < $cardBtn.length; i++) {
   });
 }
 
-const $liquidsTexts = document.querySelectorAll(".liquids__content__text");
-
-const $liquidsIcons = document.querySelectorAll(
-  ".liquids__content__type__icon"
-);
-
-for (let i = 0; i < $liquidsIcons.length; i++) {
-  const element = $liquidsIcons[i];
-  element.addEventListener("click", () => {
+for (let i = 0; i < $liquidsTypes.length; i++) {
+  const $liquidsType = $liquidsTypes[i];
+  $liquidsType.addEventListener("click", () => {
     $liquidsTexts.forEach(el => {
-      el.classList.remove("visible-block-opacity");
+      el.classList.remove("visible");
     });
-    $liquidsTexts[i].classList.toggle("visible-block-opacity");
+    $liquidsTypes.forEach(el => {
+      el.classList.remove("visible");
+    });
+    $liquidsImgsBoxs.forEach(el => {
+      el.classList.remove("visible");
+    });
+
+    $body.classList.remove("dark-mode");
+    $liquidsImgsBoxs[i].classList.add("visible");
+    $liquidsType.classList.add("visible");
+    $liquidsTexts[i].classList.toggle("visible");
+
+    if (i === 2) $body.classList.add("dark-mode");
   });
 }
