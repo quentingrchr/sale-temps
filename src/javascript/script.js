4f57676aa51551cae408bc75ac5f95dc3fc1d549
Quentin GRANCHER <quentingrancher@ANTI-SNITCH-SNITCH-CLUB.local>
2020-02-01 20:27:06 +0100

    make it rain in dev

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,12 +1,13 @@
 const $cards = document.querySelectorAll(".card");
 const $cardBtn = document.querySelectorAll(".card__btn");
 const $body = document.querySelector("body");
+const $liquidsSection = document.querySelector(".liquids");
 const $liquidsTexts = document.querySelectorAll(".liquids__content__text");
 const $liquidsTypes = document.querySelectorAll(".liquids__content__type");
 const $liquidsImgsBoxs = document.querySelectorAll(".liquids__content__box");
-
 const $iconMenu = document.querySelector("#icon-menu");
 const $menu = document.querySelector("#menu");
+const $exemple = document.querySelector("#exemple");
 
 for (let i = 0; i < $cardBtn.length; i++) {
   const element = $cardBtn[i];
@@ -27,17 +28,47 @@ for (let i = 0; i < $liquidsTypes.length; i++) {
     $liquidsImgsBoxs.forEach(el => {
       el.classList.remove("visible");
     });
+    $exemple.classList.remove("exemple--basic");
+    $exemple.classList.remove("exemple--hydro");
+    $exemple.classList.remove("exemple--fluo");
 
+    $liquidsSection.classList.remove("rain");
     $body.classList.remove("dark-mode");
     $liquidsImgsBoxs[i].classList.add("visible");
     $liquidsType.classList.add("visible");
     $liquidsTexts[i].classList.toggle("visible");
 
+    if (i == 0) {
+      //BASIC
+      $exemple.classList.toggle("exemple--basic");
+    }
+    if (i === 1) {
+      // HYDRO
+      $liquidsSection.classList.add("rain");
+      $exemple.classList.toggle("exemple--hydro");
+      // createRain();
+    }
     if (i === 2) {
+      // FLUO
       $body.classList.add("dark-mode");
+      $exemple.classList.toggle("exemple--fluo");
     }
   });
 }
+
+$iconMenu.addEventListener("click", () => {
+  // MENU BURGER GROWING
+  $menu.classList.toggle("menu-is-opened");
+});
+
+var wpLiquids = new Waypoint({
+  element: $liquidsSection,
+  handler: function(direction) {
+    $exemple.classList.toggle("opacity-1");
+  },
+
+  offset: "60%"
+});
 
 var span = document.querySelector(".hero-left__uptitle__rotate");
 
@@ -59,7 +90,25 @@ setInterval(
   5000,
   addClassDown()
 );
-$iconMenu.addEventListener("click", () => {
-  $menu.classList.toggle("menu-is-opened");
-  console.log("pute");
-});
+
+// RAIN ANIMATION
+// number of drops created.
+// var nbDrop = 858;
+
+// // function to generate a random number range.
+// function randRange(minNum, maxNum) {
+//   return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+// }
+
+// // function to generate drops
+// function createRain() {
+//   for (i = 1; i < nbDrop; i++) {
+//     var dropLeft = randRange(0, 1600);
+//     var dropTop = randRange(-1000, 1400);
+
+//     $(".rain").append('<div class="drop" id="drop' + i + '"></div>');
+//     $("#drop" + i).css("left", dropLeft);
+//     $("#drop" + i).css("top", dropTop);
+//   }
+// }
+// // Make it rain
