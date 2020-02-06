09cc0d38ce52f0bde05bfdd3b72402db4d35e5a0
RedaHamouche <reda.hamouche@outlook.com>
2020-02-06 15:04:21 +0100

    responsive tach

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -39,8 +39,6 @@ window.addEventListener("resize", () => {
   $tacheYellow.style.left = `${window.innerWidth - 200}px`;
   if (window.innerWidth < 900) {
     $tacheYellow.style.display = "none";
-  } else {
-    $tacheYellow.style.display = "block";
   }
 });
 $iconBasicArea.addEventListener("click", () => {
